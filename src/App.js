4746e3da44be29973b042ecaa16d9c5c11654bbb
Rahul Kumar <rir7890@gmail.com>
2023-07-24 18:26:09 +0530

    Dave Project Challenge to show the content of the api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useRef } from "react";
 
 function App() {
+  const showRef = useRef();
+  const userRef = useRef();
+  const postsRef = useRef();
+  const albumsRef = useRef();
+  const handleClickUsers = async () => {
+    showRef.current.innerHTML = "";
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await response.json();
+    console.log(data);
+    const lists = data
+      .map((item) => `<li>${JSON.stringify(item)}</li>`)
+      .join(" ");
+    console.log(lists);
+    showRef.current.innerHTML = lists;
+    // console.log(showRef.current.innerHTML);
+    userRef.current.classList.add("Clicked");
+    postsRef.current.classList.remove("Clicked");
+    albumsRef.current.classList.remove("Clicked");
+  };
+
+  const handleClickPosts = async () => {
+    showRef.current.innerHTML = "";
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const data = await response.json();
+    console.log(data);
+    const lists = data
+      .map((item) => `<li>${JSON.stringify(item)}</li>`)
+      .join(" ");
+    console.log(lists);
+    showRef.current.innerHTML = lists;
+    // console.log(showRef.current.innerHTML);
+    userRef.current.classList.remove("Clicked");
+    postsRef.current.classList.add("Clicked");
+    albumsRef.current.classList.remove("Clicked");
+  };
+
+  const handleClickAlbums = async () => {
+    showRef.current.innerHTML = "";
+    const response = await fetch("https://jsonplaceholder.typicode.com/albums");
+    const data = await response.json();
+    console.log(data);
+    const lists = data
+      .map((item) => `<li>${JSON.stringify(item)}</li>`)
+      .join(" ");
+    console.log(lists);
+    showRef.current.innerHTML = lists;
+    // console.log(showRef.current.innerHTML);
+    userRef.current.classList.remove("Clicked");
+    postsRef.current.classList.remove("Clicked");
+    albumsRef.current.classList.add("Clicked");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="BtnShow">
+        <button ref={userRef} onClick={handleClickUsers} className="Users">
+          User Informations
+        </button>
+        <button ref={postsRef} onClick={handleClickPosts} className="Posts">
+          Post Informations
+        </button>
+        <button ref={albumsRef} onClick={handleClickAlbums} className="Albums">
+          Album Informations
+        </button>
+      </div>
+      <div ref={showRef} className="ShowResult"></div>
     </div>
   );
 }
